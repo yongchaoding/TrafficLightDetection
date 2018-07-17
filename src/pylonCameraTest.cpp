f70a51f761f3d565c0af89d302af9ac807f05112
DingYongchao <yongchaoding@outlook.com>
2018-07-17 19:21:51 +0800

    Add PylonCameraTest function

---
+++ src/pylonCameraTest.cpp
@@ -0,0 +1,17 @@
+#include "pylonCamera.hpp"
+
+#include "time.h"
+#include "stdlib.h"
+#include <string.h>
+#include <fstream>
+#include <unistd.h>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+	pyloncamera PylonCamera;
+	while(1){
+		usleep(50*1000);
+	}
+}
